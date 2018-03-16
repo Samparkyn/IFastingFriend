93a09fd6b4df7f1b95761b022d9e7003d4a064bb
Samparkyn <samparkyn101@gmail.com>
2018-03-16 15:57:46 +0100

    Add Start, Stop & Reset functionality/buttons

--- components/timer.js
+++ components/timer.js
@@ -9,20 +9,34 @@ export default class FastingTimer extends Component {
     super(props);
     this.state = {
       totalDuration: 90000,
-      timerStart: false
+      timerStart: false,
+      timerReset: false
     };
-    // this.toggleTimer = this.toggleTimer.bind(this);
+    this.toggleTimer = this.toggleTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+    
   }
   
-  // toggleTimer() {
-  //   this.setState({timerStart: !this.state.timerStart, timerReset: false});
-  // }
+  toggleTimer() {
+    this.setState({timerStart: !this.state.timerStart, timerReset: false});
+  }
+  
+  resetTimer() {
+    this.setState({timerStart: false, timerReset: true});
+  }
     
   render() {
     return (
       <View>
         <Text>This is the timer component</Text>
-        <Timer totalDuration={this.state.totalDuration} start={true} />
+        <Timer totalDuration={this.state.totalDuration} msecs start={this.state.timerStart}
+          reset={this.state.timerReset} />
+        <TouchableHighlight onPress={this.toggleTimer}>
+          <Text style={{fontSize: 30}}>{!this.state.timerStart ? "Start" : "Stop"}</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this.resetTimer}>
+          <Text style={{fontSize: 30}}>Reset</Text>
+        </TouchableHighlight>
       </View>
     );
   }
